5497dfa10db3c89e622d350688ce2a4dab600aa1
AdityaSaksena <30adityas@students.harker.org>
2026-03-12 18:01:42 -0700

    Print fullname in upper, lower, and title case

---
+++ ch01/Exercise2-4.py
@@ -0,0 +1,4 @@
+fullname=input("What is your full name?")
+print (f"Your full name is {fullname.lower()}")
+print (f"Your full name is {fullname.upper()}")
+print (f"Your full name is {fullname.title()}")
