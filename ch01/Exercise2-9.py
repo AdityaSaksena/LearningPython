883b45d75932a1c8ecbc25288ccbe9c04e35e6d8
AdityaSaksena <30adityas@students.harker.org>
2026-03-17 18:02:46 -0700

    Add comments

--- ch01/Exercise2-9.py
+++ ch01/Exercise2-9.py
@@ -1,8 +1,8 @@
 x=16
-print(x+8)
-print(x-8)
-print(x*8)
-print(x/8)
-print(x**8)
-print(x//8)
-print(x%8)
+print(x+8) # Addition
+print(x-8) # Subtraction
+print(x*8) # Multiplication
+print(x/8) # Division
+print(x**8) # Exponentiation
+print(x//8) # Floor Division
+print(x%8) # Modulus
