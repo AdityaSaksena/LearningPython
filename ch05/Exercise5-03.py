beaee76215d9156b64df63c2c0e474dfefcda68e
AdityaSaksena <30adityas@students.harker.org>
2026-04-21 17:06:19 -0700

    Make finding the prime numbers more efficient

--- ch05/Exercise5-03.py
+++ ch05/Exercise5-03.py
@@ -2,7 +2,9 @@ number = int(input("Enter a number: "))
 if number < 2:
      print(f"{number} is not a prime number.")
 else: 
-    for denominator in range(2, int(number**0.5) + 1):
+    for denominator in range(1, int(number**0.5) + 1,2):
+        if denominator == 1:
+            denominator = 2
         if number % denominator == 0:
             print(f"{number} is not a prime number.")
             break
