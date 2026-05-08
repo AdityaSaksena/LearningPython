aa1b95e77e8785b724dc79fab13c4e29e708c88a
AdityaSaksena <30adityas@students.harker.org>
2026-05-07 17:59:48 -0700

    Print foods that people like with .join and set

--- ch06/Exercise6-04.py
+++ ch06/Exercise6-04.py
@@ -3,6 +3,8 @@ for i in range(5):
     name = input("What is your name? ")
     food = input("What is your favorite food? ")
     favfood[name] = food
-    
+
 for name in favfood:
     print(name+"'s favorite food is", favfood[name])
+
+print("People like:", ", ".join(set(favfood.values())))
