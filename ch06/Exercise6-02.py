0cbf1bd0b1c650bb00ec11fe2140564bf9198e3a
AdityaSaksena <30adityas@students.harker.org>
2026-05-07 17:27:34 -0700

    Make a dictionary of usernames and find which usernames correlate to which actual names

---
+++ ch06/Exercise6-02.py
@@ -0,0 +1,7 @@
+usernames={}
+usernames["aditya1123"] = "Aditya Saksena"
+usernames["john34881"] = "John Doe"
+usernames["jane123"] = "Jane Doe"
+
+for username in usernames:
+    print(username, ":", usernames[username])
