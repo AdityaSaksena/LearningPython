137b7bc3e47ffc2b957c1cce990d1ea3d0a84c6f
AdityaSaksena <30adityas@students.harker.org>
2026-05-07 17:38:19 -0700

    Make a dictionary of people's names and food and print each of their names with the corresponding food

---
+++ ch06/Exercise6-04.py
@@ -0,0 +1,8 @@
+favfood = {}
+for i in range(5):
+    name = input("What is your name? ")
+    food = input("What is your favorite food? ")
+    favfood[name] = food
+    
+for name in favfood:
+    print(name+"'s favorite food is", favfood[name])
